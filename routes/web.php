490c518d94712ca9ec101937ffd821776c56c269
Bela Proinsias <belaskk@outlook.com>
2026-01-04 23:11:36 +0800

    day21

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
+Route::view('/contact', 'contact');
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::resource('jobs', JobController::class);
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+// Auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
